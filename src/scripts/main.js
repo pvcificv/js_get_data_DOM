cf3d8fee0dbfb4efdd596240a5517eae959f3230
pvcificv <pvcificv@quantum.gate>
2025-09-15 18:08:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write your code here
+const nodes = document.querySelectorAll('.population');
+const populations = [...nodes].map(function (el) {
+  return Number(el.textContent.replace(/\D/g, ''));
+});
+
+const total = populations.reduce((a, b) => a + b, 0);
+const average = total / populations.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString('en-US');
+
+document.querySelector('.average-population').textContent =
+  Math.round(average).toLocaleString('en-US');
